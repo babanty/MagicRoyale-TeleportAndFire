9064f4d37ac4c821d2bc30e54294473dca5fba5e
Bosbobos <konovalov.matvej@mail.ru>
2020-03-17 22:59:00 +0300

    Добавлена БД

--- MagicRoyale-TeleportAndFire.App/Program.cs
+++ MagicRoyale-TeleportAndFire.App/Program.cs
@@ -3,17 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace MagicRoyale_TeleportAndFire.App
 {
-    public class Program
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Models.User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=master;Trusted_Connection=True;");
+        }
+    }
+
+public class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // создаем два объекта User
+                Models.User user1 = new Models.User { Name = "Tom" };
+                Models.User user2 = new Models.User { Name = "Alice" };
+
+                // добавляем их в бд
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (Models.User u in users)
+                {
+                    Console.WriteLine(u.Name);
+                }
+            }
+            Console.Read();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
